2d679a7203a8b00d5f4d0ef589b9b905f08a5157
pereladov <aleksandr.pereladov@simbirsoft.com>
2022-12-26 12:22:52 +0500

    hw7

---
+++ hw7/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+int main() {
+
+    //task1
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << 5*i << std::endl;
+    }
+
+    //task2
+
+    int val;
+    std::cout<<"Введите число: " ; //На экран выводится 'X = '
+    std::cin >> val;
+
+
+    //больше или меньше это число 7-ми.
+
+    if (val > 7) {
+        std::cout << val << " - больше 7-ми \n";
+    } else if (val == 7) {
+        std::cout << val << " - ранво 7-ми \n";
+    } else {
+        std::cout << val << "- меньше 7-ми \n";
+    }
+
+    if (val > 10) {
+        std::cout << val << " - больше 10-ти \n";
+    } else if (val == 10) {
+        std::cout << val << " - ранво 10-ти \n";
+    } else {
+        std::cout << val << "- меньше 10-ти \n";
+    }
+
+    if (val%2) {
+        std::cout << val << "- не делится на 2 \n";
+    } else {
+        std::cout << val << " - делится на 2 \n";
+    }
+
+
+    if (val%3) {
+        std::cout << val << "- не делится на 3 \n";
+    } else {
+        std::cout << val << " - делится на 3 \n";
+    }
+
+
+}
