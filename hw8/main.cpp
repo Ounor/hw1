6edac1950629daa54a25e393102d6a329224d52e
Aleksandr <53820336+Ounor@users.noreply.github.com>
2022-12-28 13:33:22 +0500

    Update main.cpp

--- hw8/main.cpp
+++ hw8/main.cpp
@@ -5,10 +5,10 @@ int main()
 {
     int i;
     int sum = 0;
-    for (i = 1; i <= 10; i++)
+    for (i = 1; i <= 100; i++)
     {
         sum = sum + i;
     }
-    std::cout << "Сумма чисел от 1 до 10 = " << sum;
+    std::cout << "Сумма чисел от 1 до 100 = " << sum;
     return 0;
 }
