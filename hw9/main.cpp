1027b5d7515c0754c94fa6eefb45486e400e6cfc
Aleksandr <53820336+Ounor@users.noreply.github.com>
2023-01-05 15:46:27 +0500

    Create main.cpp

---
+++ hw9/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int quantity = 0;
+    int sum = 0;
+
+    cout << "Количество элементов: ";
+    cin >> quantity;
+ 
+    int *arrVal = new int[quantity];
+ 
+    for (int i = 0; i < quantity; i++)
+    {
+        cout << " " << i + 1 << "-ый элемент - ";
+        cin >> arrVal[i];
+    }
+ 
+    int minVal = arrVal[0];
+    int maxVal = arrVal[0];
+   
+ 
+    for (int i = 0; i < quantity; i++)
+    {
+        if (arrVal[i] < minVal)
+        {
+            minVal = arrVal[i];
+        }
+        if (arrVal[i] > maxVal)
+        {
+            maxVal = arrVal[i];
+        }
+        sum += arrVal[i];
+    }
+ 
+     int average = sum / quantity;
+
+    cout << "Сумма элементов - " << average << " \n";
+    cout << "Среднее арифметическое элементов - " << average << " \n";
+    
+    cout << "Максимумальное число - " << maxVal << " \n";
+    cout << "Минимальное число - " << minVal << " \n";
+
+ 
+    delete[] arrVal;
+}
