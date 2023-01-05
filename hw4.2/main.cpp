254f6f71bf2df86c234460f0a1425b11a3eb966f
Aleksandr <53820336+Ounor@users.noreply.github.com>
2023-01-05 16:03:24 +0500

    Create main.cpp

---
+++ hw4.2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+ 
+int main()
+{
+
+  //task 1
+  
+    int arr[7] = {
+        1, 2, 3,4,5,6,7
+    };  
+  
+    for (int i = 0; i < 7; i++) {
+        std::cout << arr[i] << std::endl;
+    }
+
+}
