71f1c53c447c7fd2b7ff88b752557b3056033486
Aleksandr <53820336+Ounor@users.noreply.github.com>
2023-01-05 16:28:26 +0500

    Update main.cpp

--- hw4.2/main.cpp
+++ hw4.2/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
- 
+ using namespace std;
+
 int main()
 {
 
@@ -10,8 +11,23 @@ int main()
         1, 2, 3,4,5,6,7
     };  
   
-    for (int i = 0; i < 7; i++) {
-        std::cout << arr[i] << std::endl;
+    for (int i = 0; i < sizeof(arr)/sizeof(*arr); i++) {
+        cout << arr[i] << endl;
     }
 
+  //task2
+
+    int lenght;
+    cout << "Введите длину массива: ";
+    cin >> lenght;
+    int randArray[lenght];
+  
+    for(int i=0;i<lenght;i++) {
+      randArray[i]=rand()%10;
+    }
+  
+    for(int i=0;i<lenght;i++) {
+          cout << randArray[i] << " ";
+    }
+  
 }
