295ee13e69003f768c000b964d47f5bcd8d3820c
Aleksandr <53820336+Ounor@users.noreply.github.com>
2023-01-10 21:38:54 +0500

    Update main.cpp

--- hw4.3/main.cpp
+++ hw4.3/main.cpp
@@ -3,7 +3,7 @@
     cout << "Введите длину массива: ";
     cin >> lenght;
   
-    int randArray[lenght];
+    int randArray[100];
   
     for(int i=0;i<lenght;i++) {
      cout << "Введите число для добавления в массив: ";
@@ -16,3 +16,24 @@
     }
 
 
+    int temp; // временная переменная для обмена элементов местами
+
+    // Сортировка массива пузырьком
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // меняем элементы местами
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    // Вывод отсортированного массива на экран
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    delete [] arr; // освобождение памяти;
