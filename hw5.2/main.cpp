f5484a31b315d9f2059dbb7f5c4fcbee635f3fb5
Aleksandr <53820336+Ounor@users.noreply.github.com>
2023-01-26 19:09:30 +0500

    Update main.cpp

--- hw5.2/main.cpp
+++ hw5.2/main.cpp
@@ -3,27 +3,36 @@ using namespace std;
 
 int main() {
     int rows, columns;
-    cout << "Enter the number of rows:"    cin >> rows;
-    cout << "Enter the number of columns:"    cin >> columns;
+    cout << "Введите количество строк:";
+    cin >> rows;
+    cout << "Введите количество столбцов: ";
+    cin >> columns;
 
-    int arr[rows][columns];
+    int** arr = new int*[rows]; 
+    for(int i = 0; i < rows; i++) {
+        arr[i] = new int[columns];
+    }
 
-    // Input array elements
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < columns; j++) {
-            cout << "Enter element at position" << i << j
+            cout << "Введите элемент массива в положение [" << i << "][" << j << "]";
             cin >> arr[i][j];
         }
     }
 
-    // Print the array
-    cout << "The 2D array is: " << endl;
+
+    cout << "Двумерный массив: " << endl;
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < columns; j++) {
             cout << arr[i][j] << " ";
         }
         cout << endl;
     }
+
+    for(int i = 0; i < rows; i++) {
+        delete[] arr[i];
+    }
+    delete[] arr;
 
     return 0;
 }
