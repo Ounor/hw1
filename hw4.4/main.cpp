3859df195f56ca073d21662df6c42aff516b5368
Aleksandr <53820336+Ounor@users.noreply.github.com>
2023-01-26 18:48:14 +0500

    Create main.cpp
    hw4.4

---
+++ hw4.4/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Введите кол-во элементов в массиве ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Введите элемент массива: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int minVal = arr[0], maxVal = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < minVal) {
+            minVal = arr[i];
+        }
+        if (arr[i] > maxVal) {
+            maxVal = arr[i];
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    double mean = (double)sum / n;
+
+    cout << "Минимальное значение: " << minVal << endl;
+    cout << "Максимальное значение: " << maxVal << endl;
+    cout << "Сумма всех элементов: " << sum << endl;
+    cout << "Среднее арифметическое: " << mean << endl;
+
+    return 0;
+}
