ba6e9dc4e57eb2ef738b6c7b92f92fcf8ed84d22
Aleksandr <53820336+Ounor@users.noreply.github.com>
2023-02-12 00:43:40 +0500

    Update main.cpp

--- hw4.4/main.cpp
+++ hw4.4/main.cpp
@@ -1,38 +1,46 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
     cout << "Введите кол-во элементов в массиве ";
     cin >> n;
 
-    int arr[n];
-    for (int i = 0; i < n; i++) {
+    int* arr = new int[n];
+
+    cout << "Введите все элементы массива: " << endl;
+    for (int i = 0; i < n; i++)
+    {
         cout << i + 1 << "-ый элемент: ";
         cin >> arr[i];
     }
 
-    int minVal = arr[0], maxVal = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (arr[i] < minVal) {
-            minVal = arr[i];
-        }
-        if (arr[i] > maxVal) {
-            maxVal = arr[i];
-        }
-    }
-
+    int min_val = arr[0];
+    int max_val = arr[0];
     int sum = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < min_val)
+        {
+            min_val = arr[i];
+        }
+        if (arr[i] > max_val)
+        {
+            max_val = arr[i];
+        }
         sum += arr[i];
     }
 
-    double mean = (double)sum / n;
+    int average = sum / n;
 
-    cout << "Минимальное значение: " << minVal << endl;
-    cout << "Максимальное значение: " << maxVal << endl;
+  cout << "Минимальное значение: " << min_val << endl;
+    cout << "Максимальное значение: " << max_val << endl;
     cout << "Сумма всех элементов: " << sum << endl;
-    cout << "Среднее арифметическое: " << mean << endl;
+    cout << "Среднее арифметическое: " << average << endl;
+
+    delete[] arr;
 
     return 0;
 }
